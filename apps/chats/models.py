7c50ad282968ed899006c233f1c2074595553a66
kilodayt <kilodayt@mail.ru>
2026-02-03 18:22:26 +0300

    chat and message models

--- apps/chats/models.py
+++ apps/chats/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Название чата"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    text = models.TextField(verbose_name="Текст сообщения")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
