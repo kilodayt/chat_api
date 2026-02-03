0d0fde81726d3a8d3ecc12d0b7144245ce637c9d
kilodayt <kilodayt@mail.ru>
2026-02-03 20:10:26 +0300

    Field -> Annotated to avode warnings

--- apps/chats/schemas.py
+++ apps/chats/schemas.py
@@ -5,26 +5,20 @@ title: не пустой, длина 1..200
 
 text: не пустой, длина 1..5000
 """
-from pydantic import BaseModel, Field, field_validator
+from typing import Annotated
+
+from pydantic import BaseModel, StringConstraints
 
 
 class ChatCreateSchema(BaseModel):
-    title: str = Field(..., min_length=1, max_length=200, strip_whitespace=True)
-
-    @field_validator('title')
-    def validate_title(cls, v):
-        stripped = v.strip()
-        if not stripped:
-            raise ValueError("Название чата не может состоять из пробелов")
-        return stripped
+    title: Annotated[
+        str,
+        StringConstraints(strip_whitespace=True, min_length=1, max_length=200)
+    ]
 
 
 class MessageCreateSchema(BaseModel):
-    text: str = Field(..., min_length=1, max_length=5000, strip_whitespace=True)
-
-    @field_validator('text')
-    def validate_text(cls, v):
-        stripped = v.strip()
-        if not stripped:
-            raise ValueError("Сообщение не может быть пустым")
-        return stripped
+    text: Annotated[
+        str,
+        StringConstraints(strip_whitespace=True, min_length=1, max_length=5000)
+    ]
